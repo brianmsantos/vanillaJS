a149ce2e53e293d0f6ba137c5b24ef5a9221ab90
Brian Santos <brianmsantos81@gmail.com>
2019-07-01 21:27:12 -0700

    ease function added

--- smoothScroll/app.js
+++ smoothScroll/app.js
@@ -4,9 +4,25 @@ function smoothScroll (target, duration){
     var startPosition = window.pageYOffset;
     var distance = targetPosition - startPosition;
     var startTime = null;
-
-
     console.log(distance);
+
+    function animation(currentTime){
+        if(startTime === null) startTime = currentTime;
+        var timeElapsed = currentTime - startTime;
+        var run = ease(timeElapsed, startPosition, distance, duration);
+        window.scrollTo(0,)
+    }
+    
+    function ease(t, b, c, d) {
+        t /= d / 2;
+        if (t < 1) return c / 2 * t * t * b;
+        t--;
+        return -c / 2 * (t *(t - 2) - 1) + b;
+    }
+
+    requestAnimationFrame(animation)
 }
+
+
 
 smoothScroll('.section2', 1000)
